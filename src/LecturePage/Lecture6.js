91df7848a62449d3d403f8e4a20ca5f7bb0fc9e2
大石航平 <ooishikouhei@ooishikouheis-Mac-mini.local>
2022-01-14 12:43:52 +0900

    change

--- src/LecturePage/Lecture6.js
+++ src/LecturePage/Lecture6.js
@@ -204,10 +204,12 @@ const Lecture6 = (props) => {
         </div>
       </div>
 
+    const courseMaterial =  LecturesInfo[lectureId].lecture.courseMaterial
+
     const disclaimer =
     <div className="lecture-detail d-flex flex-column">
       <ul>
-        <li>書籍は、参加者各自でご用意ください。</li>
+        <li>`${courseMaterial}`</li>
         <li>講義にはWeb会議サービスの<a href="https://zoom.us/jp-jp/meetings.html"><u>Zoom</u></a>を使用いたします。
         </li>
         <li><b>各講義は、録画のうえ参加者へ講義後に共有いたしますので、一部講義にご参加が難しい場合もご参加をいただけます。</b>
